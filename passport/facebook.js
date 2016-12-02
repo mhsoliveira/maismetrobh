89fa8d2caa3b81f3e86c9261aa8c6f629d175637
mhsoliveira <hsoliveira.matheus@gmail.com>
2016-12-02 13:13:52 +0000

    login cld

--- passport/facebook.js
+++ passport/facebook.js
@@ -1,8 +1,9 @@
 var FacebookStrategy = require('passport-facebook').Strategy;
 var User = require('../models/account');
 var fbConfig = require('../fb.js');
-var image_downloader = require('image-downloader');
-
+var cloudinary = require('cloudinary');
+var cdConfig = require('../cloud.js');
+cloudinary.config(cdConfig);
 
 module.exports = function(passport) {
 
@@ -15,9 +16,6 @@ module.exports = function(passport) {
 
     // facebook will send back the tokens and profile
     function(access_token, refresh_token, profile, done) {
-
-    	console.log('profile', profile);
-
 		// asynchronous
 		process.nextTick(function() {
 
@@ -33,37 +31,21 @@ module.exports = function(passport) {
 	            if (user) {
 	                return done(null, user); // user found, return that user
 	            } else {
-
-                options = {
-                    url: profile.photos[0].value,
-                    dest: './public/avatars/'+profile.id+'.jpg',        // Save to /path/to/dest/photo.jpg
-                    done: function(err, filename, image) {
-                        if (err) {
-                            throw err;
-                        }
-                        console.log('File saved to', filename);
-                    },
-                };
-                image_downloader(options);
-	                // if there is no user found with that facebook id, create them
-	                var newUser = new User();
-
-					// set all of the facebook information in our user model
-	                newUser.access_token = access_token; // we will save the token that facebook provides to the user
+                var newUser = new User();
+                cloudinary.uploader.upload(profile.photos[0].value, function(result) {
+                  newUser.picture = result.url;
+                  newUser.access_token = access_token; // we will save the token that facebook provides to the user
 	                newUser.username  = profile.name.givenName+' '+profile.name.familyName; // look at the passport user profile to see how names are returned
-	                newUser.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
-                  newUser.picture = '../avatars/'+profile.id+'.jpg';
+	                newUser.email = profile.emails[0].value;
+                  newUser.save(function(err) {
+                      if (err)
+                          throw err;
 
-					// save our user to the database
-	                newUser.save(function(err) {
-	                    if (err)
-	                        throw err;
-
-	                    // if successful, return the new user
-	                    return done(null, newUser);
-	                });
-	            }
-
+                      // if successful, return the new user
+                      return done(null, newUser);
+                  });
+                }, { public_id: profile.id });
+              }
 	        });
         });
 
